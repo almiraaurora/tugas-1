b130487517f0fc5b8f6e0bc6717383ebcc9e952c
almiraaurora <71681697+almiraaurora@users.noreply.github.com>
2020-09-22 13:57:51 +0700

    Add files via upload

---
+++ main pemlan tugas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("BIODATA\n");
+	printf("nama: almira aurora prameswaty\n");
+	printf("NPM: 19081010137\n");
+	printf("prodi: Teknik informatika\n");
+	printf("TTL: Samarinda,10 mei 2001\n");
+	printf("alamat: perum kpn blok C3 no.7 Nunukan,Kalimantan Utara\n");
+	printf("email: almiraprameswaty@gmail.com");
+	return 0;
+}
